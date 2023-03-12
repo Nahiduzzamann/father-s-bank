d7e7079bb71e1fecb32c516b9fbc25c11a8f1c14
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-03-12 17:57:56 +0600

    update

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     const email = document.getElementById('in-email').value;
     const password = document.getElementById('in-password').value;
 
-    if (email === 'nahid' && password === '1111') {
+    if (email === 'nahid@gmail.com' && password === '1111') {
         window.location.href = 'bank.html';
     }
     else {
